c39a69eb1f2a50fc36acc2d58f3d0759e7f5c3ca
Alex Meddin <ameddin73@gmail.com>
2023-03-14 15:22:56 -0700

    infra: key vault permissions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,6 +31,26 @@ resource "azurerm_key_vault" "how-ai" {
   resource_group_name = azurerm_resource_group.how-ai.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Set",
+      "List",
+      "Delete",
+    ]
+  }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_user_assigned_identity.how-ai.principal_id
+
+    secret_permissions = [
+      "Get",
+    ]
+  }
+
   sku_name = "standard"
 }
 
