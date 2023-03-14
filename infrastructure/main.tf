78cf113f238bd79e1f7835db95ef849cc61fc6ca
Alex Meddin <ameddin73@gmail.com>
2023-03-14 15:10:21 -0700

    infra: secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,6 +19,27 @@ resource "azurerm_service_plan" "how-ai" {
   sku_name            = "Y1"
 }
 
+resource "azurerm_user_assigned_identity" "how-ai" {
+  name                = "how-ai"
+  resource_group_name = azurerm_resource_group.how-ai.name
+  location            = azurerm_resource_group.how-ai.location
+}
+
+resource "azurerm_key_vault" "how-ai" {
+  name                = "how-ai-key-vault"
+  location            = azurerm_resource_group.how-ai.location
+  resource_group_name = azurerm_resource_group.how-ai.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "standard"
+}
+
+resource "azurerm_role_assignment" "how-ai" {
+  scope                = azurerm_key_vault.how-ai.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.how-ai.principal_id
+}
+
 resource "azurerm_linux_function_app" "how-ai" {
   name                = "how-ai-linux-function-app"
   resource_group_name = azurerm_resource_group.how-ai.name
@@ -28,11 +49,12 @@ resource "azurerm_linux_function_app" "how-ai" {
   storage_account_access_key = azurerm_storage_account.how-ai.primary_access_key
   service_plan_id            = azurerm_service_plan.how-ai.id
 
-  site_config {}
-}
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.how-ai.id]
+  }
 
-resource "azurerm_app_service_source_control" "how-ai" {
-  app_id   = azurerm_linux_function_app.how-ai.id
-  repo_url = "https://github.com/ameddin73/how-ai/tree/main/how-ai-backend"
-  branch   = "main"
+  key_vault_reference_identity_id = azurerm_user_assigned_identity.how-ai.id
+
+  site_config {}
 }
