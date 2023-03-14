dc53b6801b953feddbe56e163973eaf1815c6f9a
Alex Meddin <ameddin73@gmail.com>
2023-03-14 14:18:41 -0700

    infra: backend config

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">=1.4.0"
 
+  backend "azurerm" {
+    resource_group_name = "tf-infra"
+    container_name      = "tfstate"
+    key                 = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -16,3 +22,4 @@ provider "azurerm" {
 }
 
 data "azurerm_client_config" "current" {}
+
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,4 +1,4 @@
 variable "azure_subscription" {
-  description = "The subscription in which we're creating our K8s cluster"
-  default     = "3b42d0eb-d6ad-441b-86d7-64edaf6a7a92"
+  default = "3b42d0eb-d6ad-441b-86d7-64edaf6a7a92"
 }
+
