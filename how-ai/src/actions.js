5183ade132ce9ca7706d03cfd44d56b0b3684014
Alex Meddin <ameddin73@gmail.com>
2023-03-14 20:13:25 -0700

    feat: convert exec pattern to clipboard

--- how-ai/src/actions.js
+++ how-ai/src/actions.js
@@ -1,8 +1,7 @@
 import os from 'os';
 import axios from 'axios';
-import readline from 'readline';
-import { spawn } from 'child_process';
 import { SERVER_HOST, getVersion, checkForUpdates } from './config.js';
+import { copy } from 'copy-paste';
 
 export async function update() {
   return checkForUpdates();
@@ -45,64 +44,11 @@ export async function command(prompt) {
     );
     const command = extractCode(response.data);
 
-    // Create readline
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      prompt: process.env.PS1 || '$',
-    });
-
-    // Write command
-    rl.prompt();
-    rl.write(` ${command} `);
-
-    // Exec command
-    rl.once('line', function(line) {
-      // Write command list
-      const cmds = getCommandList(line);
-
-      // Create spawn
-      const child = spawn(cmds[0], cmds.slice(1), {
-        stdio: 'inherit',
-        env: {
-          ...process.env,
-        },
-        shell: true,
-      });
-
-      child.on('close', (code) => {
-        process.exit(code);
-      });
-    });
-
-    // Close on escape
-    rl.input.on('keypress', function(_, key) {
-      // Close the readline interface
-      if (key.name == 'escape') {
-        rl.write('\r');
-        rl.close();
-      }
-    });
+    copy(command);
+    console.log('$ \x1b[33m%s\x1b[0m copied to clipboard!', command)
   } catch (err) {
     throw err;
   }
-}
-
-function getCommandList(command) {
-  // Capture tokens
-  const regex = /\$\([^)]+\)|"[^"]+"|\S+/g;
-  const cmds = command.match(regex);
-
-  // Enumerate env vars
-  cmds.forEach(function(str) {
-    const match = str.match(/\$([A-Za-z0-9_]+)|\${([A-Za-z0-9_]+)}/);
-    if (match) {
-      const varName = match[1] || match[2];
-      return process.env[varName];
-    }
-    return str;
-  })
-  return cmds;
 }
 
 function extractCode(sentence) {
